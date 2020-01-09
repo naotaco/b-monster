e90aa0d2095e7ef58269538986a59aecab56f173
koiketakayuki <koiketakayuki.dev@gmail.com>
2020-01-09 15:27:19 +0900

    add user agent

--- reserveLesson.js
+++ reserveLesson.js
@@ -16,7 +16,8 @@ async function reserveLesson(authToken, lesson, bagId) {
         cookie: `auth_token_web=${authToken};`,
         Accept: "application/json, text/javascript, */*; q=0.01",
         "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-        "X-Requested-With": "XMLHttpRequest"
+        "X-Requested-With": "XMLHttpRequest",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
       }
     }
   );
